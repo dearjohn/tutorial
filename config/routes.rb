08ed9cb3343563970beaeb2ca98aaaebbd59f57b
john <mail2johnwong@gmail.com>
2012-08-09 17:20:30 +0800

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Tutorial::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+
+  root :to => 'static_pages#home'
+  match '/about', to:'static_pages#about'
+  match '/help', to:'static_pages#help'
+  match '/contact', to:'static_pages#contact' 
+
 
   resources :microposts
 
@@ -56,7 +59,7 @@ Tutorial::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
