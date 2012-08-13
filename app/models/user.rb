93c3f5bb0c4cf0e19f718fd91b670585377c4678
john <mail2johnwong@gmail.com>
2012-08-13 19:26:00 +0800

    SignUp -unfinished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
 
   validates :name, :presence => true, length: { maximum: 50}
- 
+  validates :password, :presence => true, :confirmation => true
+  validates :password_confirmation, :presence => true
+
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
