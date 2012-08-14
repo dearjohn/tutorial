a2b9e69b2c7e7f79698ad075a5762bc072142b4b
john <mail2johnwong@gmail.com>
2012-08-14 17:40:07 +0800

    unfinished...sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < ApplicationController
 
 	def new
-		
 	end
 
 	def create
 		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			redirect_to user #直接跳转到user？？
+		else
+			flash.now[:error] = "Invalid email/password combintion!"
+			render 'new'
+		end
 	end
 
-	def destroy
-		
+	def destroy		
 	end
 
 end
